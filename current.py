32d4c7bceca5cdb733e7050f0b4acd67ecda32cc
Nanamaru-Koi <nepnepnanakoi@gmail.com>
2022-07-12 08:42:52 +0900

    modify current.py

--- current.py
+++ current.py
@@ -31,7 +31,11 @@ for i,arg in enumerate(sys_args):
     option = arg.split('=')
     
 
-    if option[0] == "axis1":
+    if option[0] == "type":
+        type_ver = int( option[1] )
+
+
+    elif option[0] == "axis1":
         m = p.match(option[1])
         if m == None:
             raise ValueError("Options are unsuported format")
@@ -71,7 +75,9 @@ for i,arg in enumerate(sys_args):
 
 
 par_dir = os.path.abspath(os.path.join( os.pardir ))
-all_data, length,fig_title = ngi.readtxt2(par_dir,data_offset=10)
+all_data, length,fig_title = ngi.readtxt(par_dir)
+
+#print(all_data)
 
 fig = plt.figure(0,figsize=(10,4),dpi=150)
 ax = fig.subplots(1,1)
@@ -79,54 +85,65 @@ ax = fig.subplots(1,1)
 print(len(all_data))
 sp_means = []
 
+type_ver = 0
 
 
 if not os.path.isdir("./image/current/"):
     os.makedirs("./image/current/")
 
-for each_data in all_data:
-    
-    data = each_data[:-5,:]
-    size_data = data.shape[0]
-    sp_mean = np.mean(data[:,7],axis=0)
-
-    line1, = ax.plot(data[:,0],data[:,7],color='b',zorder=0)
-    
-    
-    sp_means.append([data[size_data//2,0],sp_mean])
 
 
+if type_ver == 0:
+    ax.plot(all_data[:,0],all_data[:,7])
+    ax.set_xlabel("Time $t$ [h]",fontsize=18)
+    ax.set_ylabel("SP current",fontsize=18)
+    ax.set_ylim(axis1_min,axis1_max)
+    ax.set_xlim(0,all_data[-1,0])
 
-
-sp_means = np.array(sp_means)
-
-ax2 = ax.twinx()
-line2, = ax2.plot(sp_means[:,0],sp_means[:,1],'.r-',zorder=0.5)
-
-for i,change_time_str in enumerate(fig_title):
-    if i == 0 or i == len(fig_title)-1:
-        continue
-
-    change_time_str = change_time_str.split("h")
-    change_time = float(change_time_str[0])
-    
-    line3, = ax.plot([change_time,change_time],[axis1_min,axis1_max],'c--',zorder=1)
-
-
-
-
-ax2.set_ylabel("Average SP current",fontsize=18)
-ax2.set_ylim(axis2_min,axis2_max)
-
-ax.set_xlabel("Time $t$ [h]",fontsize=18)
-ax.set_ylabel("SP current",fontsize=18)
-ax.set_ylim(axis1_min,axis1_max)
-ax.set_xlim(0,each_data[-1,0])
-
-if "line3" in locals():
-	ax.legend([line1,line2,line3],["SP current", "Average SP current","The point exchanging a workpiece"])
 else:
-	ax.legend([line1,line2],["SP current","Average SP current"])
+    for each_data in all_data:
+        
+        data = each_data[:,:]
+        size_data = data.shape[0]
+        sp_mean = np.mean(data[:,7],axis=0)
+
+        line1, = ax.plot(data[:,0],data[:,7],color='b',zorder=0)
+        
+        
+        sp_means.append([data[size_data//2,0],sp_mean])
+
+
+
+
+    sp_means = np.array(sp_means)
+
+    ax2 = ax.twinx()
+    line2, = ax2.plot(sp_means[:,0],sp_means[:,1],'.r-',zorder=0.5)
+
+    for i,change_time_str in enumerate(fig_title):
+        if i == 0 or i == len(fig_title)-1:
+            continue
+
+        change_time_str = change_time_str.split("h")
+        change_time = float(change_time_str[0])
+        
+        line3, = ax.plot([change_time,change_time],[axis1_min,axis1_max],'c--',zorder=1)
+
+
+
+
+    ax2.set_ylabel("Average SP current",fontsize=18)
+    ax2.set_ylim(axis2_min,axis2_max)
+
+    ax.set_xlabel("Time $t$ [h]",fontsize=18)
+    ax.set_ylabel("SP current",fontsize=18)
+    ax.set_ylim(axis1_min,axis1_max)
+    ax.set_xlim(0,each_data[-1,0])
+
+    if "line3" in locals():
+        ax.legend([line1,line2,line3],["SP current", "Average SP current","The point exchanging a workpiece"])
+    else:
+        ax.legend([line1,line2],["SP current","Average SP current"])
 
 
 
